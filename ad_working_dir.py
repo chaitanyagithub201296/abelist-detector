fe3254a99d57447b6696ef77f3d8ff65e9764058
chaitanya kunapareddi <chaitu2012@gmail.com>
2021-11-20 17:43:18 -0500

    reading master db excel. reading the words to two different arrays.

--- ad_working_dir.py
+++ ad_working_dir.py
@@ -6,3 +6,49 @@ Created on Sat Nov 20 10:14:33 2021
 @author: chaitanyakunapareddi
 """
 
+import xlrd
+#package to read excel files in python
+ 
+# Give the location of the file
+loc = ("/Users/chaitanyakunapareddi/Desktop/iconsult/ad/master_db.xlsx")
+#location where the excel file is located.
+#must be changed when run in local.
+ 
+# To open Workbook
+wb = xlrd.open_workbook(loc)
+#to create an object of workbook.
+#this will help access the objects/functions for sheet.
+sheet_ab = wb.sheet_by_name('ableist')
+sheet_sg = wb.sheet_by_name('suggestions')
+#reading the two sheets.
+
+
+ableist_dictionary=[]
+#creating an array to store ableist words.
+for i in range(sheet_ab.nrows):
+    ableist_dictionary.append(sheet_ab.cell_value(i, 0))
+#iterate over the sheet to read all words.  
+
+ 
+suggestions_dictionary=[]
+#creating an array to store suggestion words.
+for i in range(sheet_sg.nrows):
+    suggestions_dictionary.append(sheet_sg.cell_value(i, 0))
+#iterate over the sheet to read all words.  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
