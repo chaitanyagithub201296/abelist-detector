6d743bd3a2817d0e80636f8a0a44ea89ed48ae57
chaitanya kunapareddi <chaitu2012@gmail.com>
2021-11-24 22:01:18 -0500

    importing master db using pandas. reading ableist and suggestion words. detecting ableist words and suggesting alternate words. checking grammar of the new sentence formed from suggestions.

--- ad_working_dir.py
+++ ad_working_dir.py
@@ -6,41 +6,55 @@ Created on Sat Nov 20 10:14:33 2021
 @author: chaitanyakunapareddi
 """
 
-import xlrd
-#package to read excel files in python
+import pandas as pd
+#importing pandas package
  
 # Give the location of the file
-loc = ("/Users/chaitanyakunapareddi/Desktop/iconsult/ad/master_db.xlsx")
+file_name = ("/Users/chaitanyakunapareddi/Desktop/iconsult/ad/master_db.xlsx")
 #location where the excel file is located.
 #must be changed when run in local.
- 
-# To open Workbook
-wb = xlrd.open_workbook(loc)
-#to create an object of workbook.
-#this will help access the objects/functions for sheet.
-sheet_ab = wb.sheet_by_name('ableist')
-sheet_sg = wb.sheet_by_name('suggestions')
-#reading the two sheets.
 
+wordsData = pd.read_excel(file_name)
 
 ableist_dictionary=[]
-#creating an array to store ableist words.
-for i in range(sheet_ab.nrows):
-    ableist_dictionary.append(sheet_ab.cell_value(i, 0))
-#iterate over the sheet to read all words.  
-
- 
-suggestions_dictionary=[]
-#creating an array to store suggestion words.
-for i in range(sheet_sg.nrows):
-    suggestions_dictionary.append(sheet_sg.cell_value(i, 0))
-#iterate over the sheet to read all words.  
+ableist_dictionary=wordsData['ablesit words']
+suggestions_dictionary=wordsData['suggestion words']
 
 
 
+text='I am an abnormal person and an addict and abnormal'
+replacement_words=[]
+words = text.split(' ')
+rep_word=[]
+for word in words:
+    for ab in ableist_dictionary:
+            if word == ab:
+                rep_word.append(word)
+                replacement_word = wordsData.loc[wordsData['ablesit words'] == word]['suggestion words']
+                replacement_word=replacement_word.tolist()
+                replacement_word=replacement_word[0].split(',')
+                replacement_words.append(replacement_word)
 
 
 
+solutiondf=pd.DataFrame()
+solutiondf['ab word']=rep_word
+solutiondf['sol word']=replacement_words
+solutiondf.drop_duplicates(subset ='ab word',keep = 'first', inplace = True)
+
+
+
+import language_tool_python
+tool = language_tool_python.LanguageToolPublicAPI('en-US')
+text2='i am atypical person impaired over control drug use'
+tool.correct(text)
+
+i=0
+for line in text2:
+        matches = tool.check(line)
+        i = i + len(matches)     
+        
+print("No. of mistakes found in document is ", i)
 
 
 
